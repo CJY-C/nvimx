fdc6b40f3989794e69414c6396fee07f0ccd9ed4
Allen Liao <wcliaw610@gmail.com>
2026-05-19 13:56:15 -0500

    feat(latex): add lsp config overrides

--- nvimx/langs/latex.nix
+++ nvimx/langs/latex.nix
@@ -8,7 +8,17 @@
   options.nvimx.preset.latex.enable = lib.mkEnableOption "latex";
   config = lib.mkIf (config.nvimx.preset.latex.enable) {
     nvimx.lsp.enable = true;
-    lsp.servers.texlab.enable = true;
+    lsp.servers.texlab = {
+      enable = true;
+      activate = true;
+      config.settings = {
+        texlab = {
+          build.onSave = true; # build with latexmk
+          chktex.onEdit = true; # lint with chktex
+        };
+      };
+    };
+
     plugins.treesitter.grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
       latex
     ];
