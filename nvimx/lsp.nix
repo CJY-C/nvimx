e4b04ecf75c771dc06b6302963f6a638653d3c8b
Jiyang Chen <masarity@outlook.com>
2026-06-23 22:31:14 +0800

    refactor(lsp): define formatOnSave option and deprecate raw formatting autocommand

--- nvimx/lsp.nix
+++ nvimx/lsp.nix
@@ -5,29 +5,19 @@
 }:
 
 {
-  options.nvimx.lsp.enable = lib.mkEnableOption "lsp";
+  options.nvimx.lsp = {
+    enable = lib.mkEnableOption "lsp";
+    formatOnSave = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable automatic format-on-save.";
+    };
+  };
 
   config = lib.mkIf (config.nvimx.lsp.enable) {
     plugins.lsp = {
       enable = true;
       inlayHints = true;
     };
-
-    extraConfigLua = ''
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lsp-format-on-save", { clear = false }),
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client and client:supports_method("textDocument/formatting") then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end,
-            })
-          end
-        end,
-      })
-    '';
   };
 }
