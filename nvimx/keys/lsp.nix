ac39ef2872e4fbf8918fbf7b2b8fcae81208c935
Jiyang Chen <masarity@outlook.com>
2026-06-23 22:30:41 +0800

    refactor(keys): wrap LSP keymaps conditionally under nvimx.lsp.enable

--- nvimx/keys/lsp.nix
+++ nvimx/keys/lsp.nix
@@ -1,19 +1,23 @@
+{ lib, config, ... }:
+
 {
-  plugins.lsp.keymaps = {
-    silent = true;
-    lspBuf = {
-      gd = "definition";
-      gr = "references";
-      gt = "type_definition";
-      gi = "implementation";
-      gls = "document_symbol";
-      gla = "code_action";
-      glh = "signature_help";
-      glr = "rename";
-      K = "hover";
-    };
-    diagnostic = {
-      gD = "open_float";
+  config = lib.mkIf config.nvimx.lsp.enable {
+    plugins.lsp.keymaps = {
+      silent = true;
+      lspBuf = {
+        gd = "definition";
+        gr = "references";
+        gt = "type_definition";
+        gi = "implementation";
+        gls = "document_symbol";
+        gla = "code_action";
+        glh = "signature_help";
+        glr = "rename";
+        K = "hover";
+      };
+      diagnostic = {
+        gD = "open_float";
+      };
     };
   };
 }
