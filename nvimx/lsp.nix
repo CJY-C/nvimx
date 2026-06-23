f8dbf0d2dab95e225ac7392146dfe993b5753201
Jiyang Chen <masarity@outlook.com>
2026-06-23 19:36:33 +0800

    fix(lsp): resolve Neovim 0.12 supports_method deprecation warning

--- nvimx/lsp.nix
+++ nvimx/lsp.nix
@@ -8,26 +8,26 @@
   options.nvimx.lsp.enable = lib.mkEnableOption "lsp";
 
   config = lib.mkIf (config.nvimx.lsp.enable) {
-    lsp = {
-      inlayHints.enable = true;
-      servers."*" = {
-        config = {
-          capabilities = {
-            textDocument = {
-              semanticTokens = {
-                multilineTokenSupport = true;
-              };
-            };
-            workspace.didChangeWatchedFiles.dynamicRegistration = true;
-          };
-          root_markers = [
-            ".git"
-            ".envrc"
-          ];
-        };
-      };
+    plugins.lsp = {
+      enable = true;
+      inlayHints = true;
     };
 
-    plugins.lspconfig.enable = true;
+    extraConfigLua = ''
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lsp-format-on-save", { clear = false }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client:supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
+    '';
   };
 }
